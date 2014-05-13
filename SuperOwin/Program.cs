d5657ee22d0aca1b0f9af6a44520ec380653b642
andrecarlucci <andrecarlucci@gmail.com>
2014-05-13 20:17:25 -0300

    Add Microsoft.Owin.SelfHost | Simple Startup

--- SuperOwin/Program.cs
+++ SuperOwin/Program.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin.Hosting;
 
 namespace SuperOwin {
     class Program {
         static void Main(string[] args) {
+            using (WebApp.Start<Startup>("http://localhost:12345")) {
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.UseWelcomePage();
         }
     }
 }
